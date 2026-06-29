57efaa279f00850abc22b641f82f7e2d591836f1
[신한7기]고대동 <gitkdd818@gmail.com>
2026-06-29 16:09:34 +0900

    [level 2] Title: H-Index, Time: 6.77 ms, Memory: 87.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int solution(int[] citations) {
+    TreeSet<Integer> set = Arrays.stream(citations).boxed().collect(Collectors.toCollection(TreeSet::new));
+		int[] count = new int[set.last() + 1];
+		for (int c : citations)
+			count[c]++;
+
+		int totalPaper = 0;
+		for (int h = set.last(); h >= 0; h--) {
+			totalPaper += count[h];
+			if (totalPaper >= h) {
+				
+				return h;
+			}
+
+		}
+        return 0;
+}
+}
